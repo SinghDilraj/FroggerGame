d61ece9425d5c69c63aefb4c5ffedcc960f26e27
Dilraj Singh <dilrajkehra@gmail.com>
2018-12-10 09:54:16 -0600

    gulp task imagemin added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const autoPrefixer = require('gulp-autoprefixer')
 const minCss = require('gulp-clean-css')
+const minImage = require('gulp-imagemin')
 
 gulp.task('css', () => {
   return gulp.src('./src/css/**/*.css')
@@ -9,6 +10,12 @@ gulp.task('css', () => {
     .pipe(gulp.dest('./dist/css/'))
 })
 
-gulp.task('default', ['css'], () => {
+gulp.task('image', () => {
+  return gulp.src('./src/images/**/*')
+    .pipe(minImage())
+    .pipe(gulp.dest('./dist/images/'))
+})
+
+gulp.task('default', ['css', 'image'], () => {
   gulp.watch('./src/css/**/*.css', ['css'])
 })
