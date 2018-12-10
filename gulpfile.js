3126433e90df1120d768879fa1d8cdd39374dae6
Dilraj Singh <dilrajkehra@gmail.com>
2018-12-10 09:58:49 -0600

    gulp task babel added

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp')
 const autoPrefixer = require('gulp-autoprefixer')
 const minCss = require('gulp-clean-css')
 const minImage = require('gulp-imagemin')
+const babel = require('gulp-babel')
 
 gulp.task('css', () => {
   return gulp.src('./src/css/**/*.css')
@@ -10,12 +11,21 @@ gulp.task('css', () => {
     .pipe(gulp.dest('./dist/css/'))
 })
 
+gulp.task('js', () => {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(gulp.dest('./dist/js/'))
+})
+
 gulp.task('image', () => {
   return gulp.src('./src/images/**/*')
     .pipe(minImage())
     .pipe(gulp.dest('./dist/images/'))
 })
 
-gulp.task('default', ['css', 'image'], () => {
+gulp.task('default', ['css', 'js', 'image'], () => {
   gulp.watch('./src/css/**/*.css', ['css'])
+  gulp.watch('./src/js/**/*.js', ['js'])
 })
