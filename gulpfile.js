c545f8a70e731d2e7ab198e1aef0225008a486c4
Dilraj Singh <dilrajkehra@gmail.com>
2018-12-10 10:41:36 -0600

    gulp task concat added and babel task bug fixed

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const autoPrefixer = require('gulp-autoprefixer')
 const minCss = require('gulp-clean-css')
 const minImage = require('gulp-imagemin')
 const babel = require('gulp-babel')
+const concat = require('gulp-concat')
 
 gulp.task('css', () => {
   return gulp.src('./src/css/**/*.css')
@@ -11,11 +12,17 @@ gulp.task('css', () => {
     .pipe(gulp.dest('./dist/css/'))
 })
 
-gulp.task('js', () => {
-  return gulp.src('./src/js/**/*.js')
+gulp.task('appjs', () => {
+  return gulp.src('./src/js/app.js')
     .pipe(babel({
       presets: ['@babel/env']
     }))
+    .pipe(gulp.dest('./src/js/babel.js'))
+})
+
+gulp.task('js', () => {
+  return gulp.src(['./src/js/resources.js', './src/js/babel.js', './src/js/engine.js'])
+    .pipe(concat('main.js'))
     .pipe(gulp.dest('./dist/js/'))
 })
 
@@ -25,7 +32,7 @@ gulp.task('image', () => {
     .pipe(gulp.dest('./dist/images/'))
 })
 
-gulp.task('default', ['css', 'js', 'image'], () => {
+gulp.task('default', ['css', 'js', 'appjs', 'image'], () => {
   gulp.watch('./src/css/**/*.css', ['css'])
-  gulp.watch('./src/js/**/*.js', ['js'])
+  gulp.watch('./src/js/**/*.js', ['appjs', 'js'])
 })
