f45a4c55cc53af96e062a6895c0c7b662ad681ec
Dilraj Singh <dilrajkehra@gmail.com>
2018-12-10 09:49:17 -0600

    gulp task clean-css added

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp')
 const autoPrefixer = require('gulp-autoprefixer')
+const minCss = require('gulp-clean-css')
 
-gulp.task('autoPrefixer', () => {
+gulp.task('css', () => {
   return gulp.src('./src/css/**/*.css')
     .pipe(autoPrefixer())
+    .pipe(minCss())
     .pipe(gulp.dest('./dist/css/'))
 })
 
-gulp.task('default', ['autoPrefixer'], () => {
-  gulp.watch('./src/css/**/*.css', ['autoPrefixer'])
+gulp.task('default', ['css'], () => {
+  gulp.watch('./src/css/**/*.css', ['css'])
 })
