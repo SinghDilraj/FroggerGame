fb8d9f91d8e68e9ba07ab8ab61b47a5caac051ef
Dilraj Singh <dilrajkehra@gmail.com>
2018-12-10 11:22:52 -0600

    gulp task uglify added

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const minCss = require('gulp-clean-css')
 const minImage = require('gulp-imagemin')
 const babel = require('gulp-babel')
 const concat = require('gulp-concat')
+const minJs = require('gulp-uglify')
 
 gulp.task('css', () => {
   return gulp.src('./src/css/**/*.css')
@@ -17,11 +18,17 @@ gulp.task('appjs', () => {
     .pipe(babel({
       presets: ['@babel/env']
     }))
-    .pipe(gulp.dest('./src/js/babel.js'))
+    .pipe(gulp.dest('./src/js/babel/'))
+})
+
+gulp.task('minjs', () => {
+  return gulp.src(['./src/js/resources.js', './src/js/babel/app.js'])
+    .pipe(minJs())
+    .pipe(gulp.dest('./src/js/min/'))
 })
 
 gulp.task('js', () => {
-  return gulp.src(['./src/js/resources.js', './src/js/babel.js', './src/js/engine.js'])
+  return gulp.src(['./src/js/min/resources.js', './src/js/min/babel/*js', './src/js/engine.js'])
     .pipe(concat('main.js'))
     .pipe(gulp.dest('./dist/js/'))
 })
@@ -32,7 +39,14 @@ gulp.task('image', () => {
     .pipe(gulp.dest('./dist/images/'))
 })
 
-gulp.task('default', ['css', 'js', 'appjs', 'image'], () => {
+// gulp.task('html', () => {
+//   return gulp.src('./src/*.html')
+//     .pipe(gulp.dest('./dist/'))
+// })
+
+gulp.task('default', ['css', 'appjs', 'minjs', 'js', 'image'], () => {
   gulp.watch('./src/css/**/*.css', ['css'])
-  gulp.watch('./src/js/**/*.js', ['appjs', 'js'])
+  gulp.watch('./src/js/app.js', ['appjs'])
+  gulp.watch('./src/js/**/*.js', ['minjs', 'js'])
+  // gulp.watch('./src/*.html', ['html'])
 })
